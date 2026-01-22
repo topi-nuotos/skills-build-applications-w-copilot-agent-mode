2e92a8a75390586d3731a9483350ba2ba6902276
Topi Nuotos <56358206+topi-nuotos@users.noreply.github.com>
2026-01-22 08:53:22 +0000

    Update settings and URLs to support Codespace environment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -28,8 +28,12 @@ SECRET_KEY = 'django-insecure-h3_%u*=2ia5+zxw6s96i241%dd&&l1pvc88j+!n#ck1$$7)*4d
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
